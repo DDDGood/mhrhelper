99514f8f795a1850378ac7a6680758266689bf1f
sammy.hsu <sammy.hsu@xrspace.io>
2021-02-05 11:31:02 +0800

    minor update

--- components/endemics_main.vue
+++ components/endemics_main.vue
@@ -11,7 +11,7 @@
         </details>
         <details open v-show="!IsNullOrEmpty(endemicdata.video)">
           <summary class="header2">使用效果影片</summary>
-          <span><p class="description-text-quote">影片來自巴哈姆特：<a href="https://forum.gamer.com.tw/C.php?bsn=5786&snA=158908"> 環境生物整理<a>，感謝版友馬雲巴巴(jimmerican55)提供影片。</P></span>
+          <span><p class="description-text-quote">影片來自巴哈姆特：<a href="https://forum.gamer.com.tw/C.php?bsn=5786&snA=158908"> 環境生物整理<a>，感謝版友jimmerican55(馬雲巴巴)提供影片。</P></span>
           <video
             ref="video"
             width="100%"
