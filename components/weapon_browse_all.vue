f519703a15fd8a7ac434d3a05b3600935c0ed3e6
sammy.hsu <sammy.hsu@xrspace.io>
2021-02-22 18:04:59 +0800

    prepare some weapon data

---
+++ components/weapon_browse_all.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="layout-main">
+    <div class="header3" :key="key">{{$t('list')}}</div>
+    <div class="link-list">
+      <router-link
+        tag="button"
+        class="link-button"
+        v-for="(item, key) in weapons"
+        :key="key"
+        :to="'/weapons/' + key"
+      >
+        <img
+          class="link-icon"
+          v-bind:src="IsNullOrEmpty(item.icon) ? 'images/monsters/icons/icon_unknown.png': item.icon "
+        />
+        <div class="link-text">{{$t('weapons.name.' + key)}}</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      //   types: {}
+    };
+  },
+  props: ["weapons"],
+  created: function () {
+
+  },
+};
+</script>  
