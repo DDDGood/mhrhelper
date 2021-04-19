19d16aefca9bb8590a6b8ecc5c1cd4348a29997b
sammy.hsu <sammy.hsu@xrspace.io>
2021-04-19 16:29:07 +0800

    update parts data

--- components/mon_main.vue
+++ components/mon_main.vue
@@ -16,8 +16,8 @@
             class="hitzone-image"
             :src="mondata.hitdata.hitzone_image"
           />
-          <table id="hitdata-table">
-            <tbody id="hitdata_table_tbody">
+          <table>
+            <tbody>
               <tr class="card-text text-bold">
                 <th>#</th>
                 <th>{{$t('monster.part')}}</th>
@@ -73,57 +73,46 @@
               </tr>
             </tbody>
           </table>
+        </details>
 
-          <!-- <table id="hitdata-table">
-            <tbody id="hitdata_table_tbody">
+        <details open v-if="mondata.partsdata!== undefined">
+          <summary class="header2">{{$t('monster.partsdata')}}</summary>
+          <img
+            v-if="IsNullOrEmpty(mondata.partsdata.parts_image) === false"
+            class="hitzone-image"
+            :src="mondata.partsdata.parts_image"
+          />
+          <table>
+            <tbody>
               <tr class="card-text text-bold">
                 <th>#</th>
                 <th>{{$t('monster.part')}}</th>
-                <th>{{$t('monster.state')}}</th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/equipments/greatsword.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/equipments/hammer.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/maps/ammo_normal.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/element/element_fire.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/element/element_water.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/element/element_thunder.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/element/element_ice.png" />
-                </th>
-                <th class="number">
-                  <img class="hitdata-icon" src="images/icons/element/element_dragon.png" />
-                </th>
+                <th>{{$t('monster.stagger')}}</th>
+                <th>{{$t('monster.break')}}</th>
+                <th>{{$t('monster.loss')}}</th>
+                <th>{{$t('monster.extract')}}</th>
               </tr>
               <tr
                 class="card-text"
-                v-for="(part,id) in mondata.hitdata.parts_new"
-                :key="part.part + part.state +　id"
+                v-for="(part,id) in mondata.partsdata.parts"
+                :key="part.part + id"
               >
-                <td :style="{width: '1%',background: part.hitzone_color}"></td>
+                <td :style="{width: '1%',background: part.part_color}"></td>
                 <td>{{ part.part }}</td>
-                <td>{{ part.condition === undefined? "通常" : part.condition }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.cut >= 45 }">{{ part.cut }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.blunt >= 45 }">{{ part.blunt }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.ammo >= 45 }">{{ part.ammo }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.fire >= 25 }">{{ part.fire }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.water >= 25 }">{{ part.water }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.thunder >= 25 }">{{ part.thunder }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.ice >= 25 }">{{ part.ice }}</td>
-                <td v-bind:class="{ 'hitdata-highlight': part.dragon >= 25 }">{{ part.dragon }}</td>
+                <td>{{ part.stagger }}</td>
+                <td>{{ part.break == undefined? "-" : "(" + part.break.length + ")" + part.break[0].vital }}</td>
+                <td>{{ part.loss == undefined? "-" : "(" + $t('monster.weakness.' + part.loss.damage_type) + ")" + part.loss.vital }}</td>
+                <td>
+                  <div class="flex-row flex-intense">
+                    <div
+                      :class="{ 'extract-red': part.extract === 'red', 'extract-orange': part.extract === 'orange', 'extract-white': part.extract === 'white'  }"
+                    ></div>
+                    <div>{{ $t('monster.extract_type.' + part.extract) }}</div>
+                  </div>
+                </td>
               </tr>
             </tbody>
-          </table>-->
+          </table>
         </details>
 
         <details open v-if="mondata.items!== undefined">
@@ -131,56 +120,8 @@
           <itemtable :items="mondata.items"></itemtable>
         </details>
 
-        <!-- <details open v-if="mondata.materials!== undefined">
-          <summary class="header2">{{$t('monster.materials')}}</summary>
-          <table id="hitdata-table">
-            <tbody id="hitdata_table_tbody">
-              <tr class="card-text text-bold">
-                <th width="50%">{{$t('low_rank')}}</th>
-                <th width="50%">{{$t('high_rank')}}</th>
-              </tr>
-              <template v-for="(box,i) in mondata.materials">
-                <tr :key="i">
-                  <td colspan="2" class="materials-source">
-                    {{box.source + (box.num === undefined? "" : "(" + box.num + ")")}}
-                    <div v-if="box.info!==undefined" class="materials-source-info">{{box.info}}</div>
-                  </td>
-                </tr>
-                <tr :key="i" class="card-text text-bold">
-                  <td>
-                    <div v-for="(item, j) in box.low_rank" :key="j" class="flex-row flex-intense">
-                      <div
-                        class="materials-text-item"
-                      >{{ $te('data.items.' + item.item + '.name')? $t('data.items.' + item.item + '.name' ) : item.item}}</div>
-                      <div class="materials-text-rate">{{item.rate === ""? "?":item.rate}}</div>
-                    </div>
-                  </td>
-                  <td>
-                    <div v-for="(item, j) in box.high_rank" :key="j" class="flex-row flex-intense">
-                      <div
-                        class="materials-text-item"
-                      >{{ $te('data.items.' + item.item + '.name')? $t('data.items.' + item.item + '.name' ) : item.item}}</div>
-                      <div class="materials-text-rate">{{item.rate === ""? "?":item.rate}}</div>
-                    </div>
-                  </td>
-                </tr>
-              </template>
-            </tbody>
-          </table>
-        </details>-->
-
         <mon_moves :monmoves="movedata" v-if="!IsNullOrEmpty(movedata)" @clickmove="OnClickMove"></mon_moves>
-        <!-- <div id="divmoves" v-if="!IsNullOrEmpty(movedata)">
-          <details open v-show="!IsNullOrEmpty(movedata.outline)">
-            <summary class="header2">{{$t('monster.battlestrategy')}}</summary>
-            <span id="moveoutline" v-html="GetDescriptionText('outline', 'move')"></span>
-          </details>
-          <details open v-show="!IsNullOrEmpty(movedata)">
-            <summary class="header2">{{$t('monster.moves')}}</summary>
-            <div class="description-text">{{$t('monster.move.moveshint')}}</div>
-            <mon_moves :monmoves="movedata"></mon_moves>
-          </details>
-        </div>-->
+
         <details v-if="!IsNullOrEmpty(mondata.detail)">
           <summary class="header2">{{$t('description_detail')}}</summary>
           <span id="detail" v-html="GetDescriptionText('detail')"></span>
@@ -347,11 +288,29 @@ tfoot {
 .hitzone-image {
   display: block;
   max-width: 80%;
-  max-height: 30%;
+  max-height: 50vh;
   margin-top: 30px;
   margin-bottom: 30px;
   margin-left: auto;
   margin-right: auto;
+}
+
+.extract-red {
+  width: 8px;
+  height: 8px;
+  border: 1px solid black;
+  background-color: orange;
+}
+.extract-white {
+  width: 8px;
+  height: 8px;
+  border: 1px solid black;
+}
+.extract-orange {
+  width: 8px;
+  height: 8px;
+  border: 1px solid black;
+  background-color: red;
 }
 
 /* mobile */
